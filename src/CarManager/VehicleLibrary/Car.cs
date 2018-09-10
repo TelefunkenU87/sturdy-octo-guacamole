0bb054d7183a3c2aea43cf0f54e7acda6d68d745
Morgan Webb <telewebb@gmail.com>
2018-09-10 10:02:17 +0700

    ASP.NET version of CarManager for CCA

---
+++ src/CarManager/VehicleLibrary/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeyMeow.VehicleLibrary
+{
+    public class Car : Vehicle
+    {
+        public int NumberOfDoors { get; set; }
+    }
+}
